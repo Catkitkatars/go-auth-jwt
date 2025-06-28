bd8bfcd9885bf017887e37498bdfcfcd3ff7dd76
peseniy <catkitkat.ars@yandex.ru>
2025-06-28 20:57:40 +0500

    init docker

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,4 +8,6 @@ func main() {
 	// Router - github.com/gin-gonic/gin
 
 	// Orm - gorm.io/gorm - router and orm too big for my project, but I want to try it
+
+	// Middleware - github.com/go-chi/jwtauth
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module cmd/app/main.go
+
+go 1.24.3
