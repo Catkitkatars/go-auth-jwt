2da7e9d6c7f3d3c62b7b36a9330eebf125cb554b
peseniy <catkitkat.ars@yandex.ru>
2025-06-28 20:05:39 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# GO AUTH JWT
+Application for authentication and authorization using JWT in Go.
+
+## Fast Start
+
+to be continued ...
+
+```bash
---
+++ cmd/app/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// ENV github.com/caarlos0/env
+
+	// Logger - log/slog
+
+	// Router - github.com/gin-gonic/gin
+
+	// Orm - gorm.io/gorm - router and orm too big for my project, but I want to try it
+}
